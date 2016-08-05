455cd249f6159da2cb799628bf429a6ca6055d2e
Aspect <flimmelj@gmail.com>
2016-08-05 10:53:47 +0300

    Add generic image for units without image

--- Client/Game/Data.cs
+++ Client/Game/Data.cs
@@ -125,7 +125,7 @@ namespace Client
                 case SharedData.UNIT_WOLF_PACK_LEADER: return Image.FromFile("res/units/wolf_pack_leader.png");
                 case SharedData.UNIT_LIEUTENANT_LANDAX: return Image.FromFile("res/units/lieutenant_landax.png");
 
-                default: return Image.FromFile("res/units/warlock/warlock_none.png");
+                default: return Image.FromFile("res/units/unknown.png");
             }
         }
 
